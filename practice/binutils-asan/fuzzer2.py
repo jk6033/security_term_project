4a3a67880ebebe82be145029fec33ae4e9c5cb74
Joohyung Kim <crybaby26@yonsei.ac.kr>
2019-06-22 02:51:59 +0900

    mutation strategy added

--- practice/binutils-asan/fuzzer2.py
+++ practice/binutils-asan/fuzzer2.py
@@ -330,7 +330,7 @@ def mutate(orig_file):
             pass
         elif strategy == 3:                          
             # print 'mutation strategy 3.'                    # arithmetic inc/dec
-            # mutate_data = simple_arithmatic(mutate_data)
+            mutate_data = simple_arithmatic(mutate_data)
             pass
         elif strategy == 4:                          
             # print 'mutation strategy 4.'                    # interesting value
